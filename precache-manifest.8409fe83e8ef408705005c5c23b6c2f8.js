3aef031295d1060a74e63782433e0299fc0e27ac
Nick_Vas <111431459+NikolasRay@users.noreply.github.com>
2022-08-26 00:24:44 +0300

    Updates

---
+++ precache-manifest.8409fe83e8ef408705005c5c23b6c2f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "17987b095a5b60c3f97b",
+    "url": "/personal-projects/static/css/main.244b3264.chunk.css"
+  },
+  {
+    "revision": "17987b095a5b60c3f97b",
+    "url": "/personal-projects/static/js/main.17987b09.chunk.js"
+  },
+  {
+    "revision": "55141515be7a0ad08bfe",
+    "url": "/personal-projects/static/js/1.55141515.chunk.js"
+  },
+  {
+    "revision": "1bebb06082dd2e9d73de",
+    "url": "/personal-projects/static/js/runtime~main.1bebb060.js"
+  },
+  {
+    "revision": "c55a4ccca312a4aa57ee27db48457b35",
+    "url": "/personal-projects/static/media/NickVasileiouCV.c55a4ccc.pdf"
+  },
+  {
+    "revision": "431fea4eb8a5c955c4ae6ae41d97ce70",
+    "url": "/personal-projects/static/media/asset5.431fea4e.jpg"
+  },
+  {
+    "revision": "98126c3fc13bc2b4d39668984aac8a6c",
+    "url": "/personal-projects/static/media/portofolio-asset1.98126c3f.png"
+  },
+  {
+    "revision": "6bb88faa6e489508a673717ceb6f524f",
+    "url": "/personal-projects/index.html"
+  }
+];
